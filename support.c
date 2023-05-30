4b9a34e616254b4090a13b72d4fee550abb8ed10
Fuufuu <ifunanyanwalusiuka@gmail.com>
2023-05-30 06:17:04 -0700

    Updated the Support File

--- support.c
+++ support.c
@@ -34,3 +34,81 @@ int pr_num(va_list arg)
 
 	return (len);
 }
+
+/**
+* rev_str - reverse string
+* @s: input
+*
+* Return: pointer
+*/
+char *rev_str(char *s)
+{
+int len, i;
+char *dest;
+char temp;
+
+if (s == NULL || *s == &#39;\0&#39;)
+return (s);
+
+for (len = 0; s[len] != &#39;\0&#39;; len++);
+
+dest = malloc(sizeof(char) * (len + 1));
+if (dest == NULL)
+return (NULL);
+_memcpy(dest, s, len);
+for (i = 0; i &lt; len; i++, len--)
+{
+temp = dest[len - 1];
+dest[len - 1] = dest[i];
+dest[i] = temp;
+}
+return (dest);
+}
+
+/**
+* _memcpy - copy memory
+* @dest: input
+* @src: input
+* @n: number of bytes
+*
+* Return: pointer
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i &lt; n; i++)
+dest[i] = src[i];
+dest[i] = &#39;\0&#39;;
+
+return (dest);
+}
+
+/**
+* base_len - calculate base lenght
+* @n: input
+* @b: base input
+*
+* Return: lenght
+*/
+unsigned int base_len(unsigned int n, int b)
+{
+unsigned int i;
+
+for (i = 0; n &gt; 0; i++)
+
+n = n / b;
+return (i);
+}
+
+/**
+* pr_base - print base number
+* @c: string input
+*
+*/
+void pr_base(char *c)
+{
+int i;
+for (i = 0; c[i] != &#39;\0&#39;; i++)
+_putchar(str[i]);
+}
