76b3ed14b27a9ddc638534fadd1ce2b1a5aa5bb6
Fuufuu <ifunanyanwalusiuka@gmail.com>
2023-05-30 07:54:02 -0700

    Adding more support file

---
+++ support_2.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+* is_digit – finds digit
+* @c: input
+* Return: 1 , else 0
+*/
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
+
+/**
+* is_print – finds char
+* @c: input
+* Return: 1 , else 0
+*/
+int is_print(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+
+	return (0);
+}
+
+/**
+* append_hex - Append ascci to buffer
+* @buffer: Array
+* @index: input
+* @code: ASSCI CODE.
+* Return: Always 3
+*/
+int append_hex(char code, char buffer[], int index)
+{
+	char aleph_tab[] = "0123456789ABCDEF";
+
+	if (code < 0)
+		code *= -1;
+
+	buffer[index++] = '\\';
+	buffer[index++] = 'x';
+
+	buffer[index++] = aleph_tab[code / 16];
+	buffer[index] = aleph_tab[code % 16];
+
+	return (3);
+}
