d35f6d30c57f549b26e1f471b6c3cf48f2850ea1
Fuufuu <ifunanyanwalusiuka@gmail.com>
2023-05-30 22:07:20 -0700

    Updated the support_3.c

--- support_3.c
+++ support_3.c
@@ -4,7 +4,7 @@
  * wr_pointer - prints memory address
  * @buffer: array
  * @ind: index
- * @len: lenght specifier
+ * @len: length specifier
  * @width: width specifier
  * @flag: flags
  * @padd: padding character
@@ -19,12 +19,11 @@ int wr_pointer(char buffer[], int ind, int len, int width,
 	int i;
 
 	if (width > len)
-
 	{
 		for (i = 3; i < width - len + 3; i++)
 			buffer[i] = padd;
 		buffer[i] = '\0';
-		if (flag && F_MINUS && padd == ' ')
+		if (flag & F_MINUS && padd == ' ')
 		{
 			buffer[--ind] = 'x';
 			buffer[--ind] = '0';
@@ -39,15 +38,15 @@ int wr_pointer(char buffer[], int ind, int len, int width,
 			if (extra)
 				buffer[--ind] = extra;
 			return (write(1, &buffer[ind], len) + write(1, &buffer[3], i - 3));
-			else if (!(flag & F_MINUS) && padd == '0')
-			{
-				if (extra)
-					buffer[--padd_start] = extra;
-				buffer[1] = '0';
-				buffer[2] = 'x';
-				return (write(1, &buffer[padd_start], i - padd_start) +
-						write(1, &buffer[ind], len - (1 - padd_start) - 2));
-			}
+		}
+		else if (!(flag & F_MINUS) && padd == '0')
+		{
+			if (extra)
+				buffer[--padd_start] = extra;
+			buffer[1] = '0';
+			buffer[2] = 'x';
+			return (write(1, &buffer[padd_start], i - padd_start) +
+					write(1, &buffer[ind], len - (1 - padd_start) - 2));
 		}
 		buffer[--ind] = 'x';
 		buffer[--ind] = '0';
@@ -55,4 +54,5 @@ int wr_pointer(char buffer[], int ind, int len, int width,
 			buffer[--ind] = extra;
 		return (write(1, &buffer[ind], BUFF_SIZE - ind - 1));
 	}
+	return (0);
 }
